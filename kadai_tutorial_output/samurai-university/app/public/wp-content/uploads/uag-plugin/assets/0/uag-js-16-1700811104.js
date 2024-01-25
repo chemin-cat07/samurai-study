59e58ff6da1213c778a13336446ef27578025708
chemin-cat07 <149939472+chemin-cat07@users.noreply.github.com>
2024-01-25 16:10:37 +0900

    samurai univercity課題提出
    完成物です

---
+++ kadai_tutorial_output/samurai-university/app/public/wp-content/uploads/uag-plugin/assets/0/uag-js-16-1700811104.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"dd2e04e0","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-dd2e04e0' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"dd2e04e0","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-dd2e04e0' );
+});
+ });
